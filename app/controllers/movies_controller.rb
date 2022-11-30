9c94f551e734affd5c552c79d9aafb1c5c111a60
Enrique Ramirez <nrqrmz@gmail.com>
2022-11-30 12:59:49 -0600

    search multiple columns using OR

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,8 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+      sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
