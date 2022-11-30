300090f77dc5e4df370dfa5d0e87860b6301dbb3
Enrique Ramirez <nrqrmz@gmail.com>
2022-11-30 16:19:20 -0600

    refactor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,13 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      sql_query = "title ILIKE :query OR synopsis ILIKE :query"
-      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+      sql_query = <<~SQL
+        title ILIKE :query
+        OR synopsis ILIKE :query
+        OR first_name ILIKE :query
+        OR last_name ILIKE :query
+      SQL
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
