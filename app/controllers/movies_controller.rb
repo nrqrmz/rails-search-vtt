b7489e760da7e1829fb307e3831343e44928b308
Enrique Ramirez <nrqrmz@gmail.com>
2022-11-30 12:46:11 -0600

    search using case insensitive and wildcards

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      @movies = Movie.where(title: params[:query])
+      @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
